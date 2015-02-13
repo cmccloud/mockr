e7d5133127620d94e14df2614aa723c3dd0be6fe
Jake Obron <jbobron@gmail.com>
2015-02-13 15:01:42 -0800

    added user db functions

--- src/server/api/user/userController.js
+++ src/server/api/user/userController.js
@@ -1,43 +1,49 @@
 var User = require('./userModel');
 
-var createUser = function (req, res) {
-  var username = req.body.username;
-  var password = req.body.password;
-  var newUser = new User({ username: username, password: password });
-  newUser.save(function (err) {
-    if (err) console.log('Error: ', err);
-    console.log('User created');
-  });
-};
+module.exports = {
+  createUser: function (req, res) {
+    var username = req.body.username;
+    var newUser = new User({ 'username': username });
+    newUser.save(function (err) {
+      if (err){
+        res.end();
+        return console.log('Error: ', err);
+        
+      } 
+      console.log('User created');
+      res.end();
+    });
+  },
 
-//TODO: return user, specify user
-var getUser = function (req, res) {
-  var username = req.params.username;
-  User.find({'username': username}, function (err, user) {
-    //specify user!!!
-    if (err) return console.log('Error: ', err);
-    //return user
-    console.log(user);
-  });
-};
+  //TODO: return user, specify user
+  getUser: function (req, res) {
+    var username = req.params.username;
+    User.find({'username': username}, function (err, user) {
+      //specify user!!!
+      if (err) return console.log('Error: ', err);
+      //return user
+      console.log(user);
+    });
+  },
 
 
-var getUsers = function (req, res) {
-  User.find(function (err, users) {
-    if (err) return console.log('Error: ', err);
-    //return users
-    console.log(users);
-  });
-};
+  getUsers: function (req, res) {
+    User.find(function (err, users) {
+      if (err) return console.log('Error: ', err);
+      //return users
+      console.log(users);
+    });
+  },
 
-//NOT COMPLETE
-//TODO: specify user, lookup format for findAndModify
-var editUser = function (req, res) {
-  var username = req.params.username;
-  User.findAndModify(User, {'username': username}, function (err, users) {
-    //specify user!!!
-    if (err) return console.log('Error: ', err);
-    //return users
-    console.log(users);
-  });
+  //NOT COMPLETE
+  //TODO: specify user, lookup format for findAndModify
+  editUser: function (req, res) {
+    var username = req.params.username;
+    User.findAndModify(User, {'username': username}, function (err, users) {
+      //specify user!!!
+      if (err) return console.log('Error: ', err);
+      //return users
+      console.log(users);
+    });
+  }
 };
