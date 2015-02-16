33835b3bec417fda38c1d401583fd1f4b9cf2ef3
Ruben V <ruben1586@gmail.com>
2015-02-16 11:31:58 -0800

    endpoint router working

--- src/server/routes.js
+++ src/server/routes.js
@@ -5,10 +5,7 @@ module.exports = function applicationRouter(app) {
   require('./api/auth')(router);
   require('./api/user')(router);
   require('./api/endpoint')(router);
-
-  router.get('/:user/:path', function(req, res, next) {
-  
-  });
+  require('./api/endpointRouter')(router);
   
   router.get('/*', function(req, res, next) {
     res.redirect('/');
